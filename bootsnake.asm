81f8030a808a36ed569872886d14ac70b5f07d2b
Pavel Serebryakov <cspk@list.ru>
2016-01-26 22:44:21 +0500

    Set ES register for video memory segment

--- bootsnake.asm
+++ bootsnake.asm
@@ -6,10 +6,16 @@ org 0x7C00
 bootsect_sz equ 512
 boot_sign_sz equ 2
 
+video_mem_segment equ 0xB800
+
 ; hide the cursor
 mov ah, bios_video_cursor_shape_fn
 mov ch, 0x20; bits 6:5 == 01 - cursor invisible
 int bios_video_int
+
+; setup video memory
+mov ax, video_mem_segment
+mov es, ax
 
 jmp $; stub loop
 
