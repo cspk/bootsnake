e98dcf03c06c9167800262564b2c283862ba9097
Pavel Serebryakov <cspk@list.ru>
2016-01-31 08:53:09 +0500

    Add "main" label and indent its code

--- bootsnake.asm
+++ bootsnake.asm
@@ -13,22 +13,22 @@ ioport_kbd equ 0x60
 ioport_pic equ 0x20
 pic_eoi equ 0x20; end of interrupt command code
 
-; hide the cursor
-mov ah, bios_video_cursor_shape_fn
-mov ch, 0x20; bits 6:5 == 01 - cursor invisible
-int bios_video_int
+main:
+	; hide the cursor
+	mov ah, bios_video_cursor_shape_fn
+	mov ch, 0x20; bits 6:5 == 01 - cursor invisible
+	int bios_video_int
 
-; setup video memory
-mov ax, video_mem_segment
-mov es, ax
+	; setup video memory
+	mov ax, video_mem_segment
+	mov es, ax
 
-; setup keyboard interrupt service routine
-cli; disable interrupts
-mov word [kbd_int * 4], kbd_isr; write the offset of the ISR to the IVT
-mov word [kbd_int * 4 + 2], cs; write the segment containing the ISR
-sti; enable interrupts
-
-jmp $; stub loop
+	; setup keyboard interrupt service routine
+	cli; disable interrupts
+	mov word [kbd_int * 4], kbd_isr; write the offset of the ISR to the IVT
+	mov word [kbd_int * 4 + 2], cs; write the segment containing the ISR
+	sti; enable interrupts
+jmp $
 
 kbd_isr:
 	push ax
